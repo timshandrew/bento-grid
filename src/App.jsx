55f741acd9e482899a6501f77b8bd717d4f46a16
Andrew <andrew.alan.sm@gmail.com>
2025-05-04 15:07:18 +0100

    add assets and begin creating tailwind theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
     return (
         <>
-            <h1 className="text-red-500">Bento Grid</h1>
+            <h1 className="text-8xl font-dm-sans-med italic">Bento Grid</h1>
         </>
     );
 }
