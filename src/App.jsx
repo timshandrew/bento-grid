fab02843e46233e7e89208e1fe036763f3e389fc
Andrew <andrew.alan.sm@gmail.com>
2025-05-05 23:57:25 +0100

    style first section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import fiveStarsImg from "./assets/images/illustration-five-stars.webp";
+
 function App() {
     return (
-        <>
-            <h1 className="font-dm-sans-med italic bg-yellow-500  text-preset-1">
-                Bento Grid
-            </h1>
-        </>
+        <div className="bg-purple-500 text-white m-1 rounded-lg p-9">
+            <div className="flex flex-col  place-items-center">
+                <h1 className="text-preset-1 text-center">
+                    Social Media <span className="text-yellow-500">10x</span>
+                    <span className="italic">Faster</span> with AI
+                </h1>
+                <img src={fiveStarsImg} />
+                <span className="text-preset-4 font-dm-sans-reg">
+                    Over 4,000 5-star reviews
+                </span>
+            </div>
+        </div>
     );
 }
 
