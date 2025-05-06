9c728dfe9bf5d71d1f220282898402f6c36f0af1
Andrew <andrew.alan.sm@gmail.com>
2025-05-06 11:27:42 +0100

    add and style fourth and fifth cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import fiveStarsImg from "./assets/images/illustration-five-stars.webp";
 import multiplePathsImg from "./assets/images/illustration-multiple-platforms.webp";
-import scheduleImg from "./assets/images/illustration-consistent-schedule.webp";
+import consistent from "./assets/images/illustration-consistent-schedule.webp";
+import scheduleImg from "./assets/images/illustration-schedule-posts.webp";
+import growthImg from "./assets/images/illustration-grow-followers.webp";
 
 function App() {
   return (
@@ -27,7 +29,23 @@ function App() {
         <h1 className="text-preset-3 mb-2">
           Maintain a consistent posting schedule.
         </h1>
-        <img src={scheduleImg} />
+        <img src={consistent} />
+      </div>
+
+      <div className="m-1 rounded-lg bg-purple-100 p-2">
+        <h1 className="text-preset-3 mb-3">Schedule to social media</h1>
+        <img className="mb-3" src={scheduleImg} />
+        <span className="text-preset-4 font-dm-sans-reg block text-center">
+          Optimize post timings to publish content at the perfect time for your
+          audience.
+        </span>
+      </div>
+
+      <div className="rounded-lg bg-purple-500 p-3 text-white">
+        <img className="mx-auto mb-5" src={growthImg} />
+        <h1 className="text-preset-2 text-center">
+          Grow followers with non-stop content.
+        </h1>
       </div>
     </div>
   );
