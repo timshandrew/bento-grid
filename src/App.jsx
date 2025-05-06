be997e86402eaf6983e9b77bc521ffc335771b28
Andrew <andrew.alan.sm@gmail.com>
2025-05-06 10:23:25 +0100

    add and style second card

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
 import fiveStarsImg from "./assets/images/illustration-five-stars.webp";
+import multiplePathsImg from "./assets/images/illustration-multiple-platforms.webp";
 
 function App() {
-    return (
-        <div className="bg-purple-500 text-white m-1 rounded-lg p-9">
-            <div className="flex flex-col  place-items-center">
-                <h1 className="text-preset-1 text-center">
-                    Social Media <span className="text-yellow-500">10x</span>
-                    <span className="italic">Faster</span> with AI
-                </h1>
-                <img src={fiveStarsImg} />
-                <span className="text-preset-4 font-dm-sans-reg">
-                    Over 4,000 5-star reviews
-                </span>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className="m-1 flex flex-col place-items-center rounded-lg bg-purple-500 p-9 text-white">
+        <h1 className="text-preset-1 text-center">
+          Social Media <span className="text-yellow-500">10x</span>
+          <span className="italic">Faster</span> with AI
+        </h1>
+        <img src={fiveStarsImg} />
+        <span className="text-preset-4 font-dm-sans-reg">
+          Over 4,000 5-star reviews
+        </span>
+      </div>
+
+      <div className="m-1 bg-white">
+        <img src={multiplePathsImg} />
+        <h1 className="text-preset-3">
+          Manage multiple accounts and platforms.
+        </h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
